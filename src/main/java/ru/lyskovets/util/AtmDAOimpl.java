2aaec731c05d87032a5a60d10cdd1ba4bc5c884e
AELyskovets <sterlingg2013@gmail.com>
2018-03-26 20:37:05 +0300

    add some DAO

--- src/main/java/ru/lyskovets/util/AtmDAOimpl.java
+++ src/main/java/ru/lyskovets/util/AtmDAOimpl.java
@@ -1,19 +1,22 @@
 package ru.lyskovets.util;
 
+import org.hibernate.HibernateException;
 import ru.lyskovets.model.CardinfoEntity;
 
 import org.hibernate.Session;
 import java.sql.SQLException;
 
+import static ru.lyskovets.util.HibernateUtil.getSessionFactory;
+
 public class AtmDAOimpl implements AtmDAO {
     public void addCardinfoEntity(CardinfoEntity cardinfoEntity) {
         Session session = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = getSessionFactory().openSession();
             session.beginTransaction();
             session.save(cardinfoEntity);
             session.getTransaction().commit();
-        } catch (Exception ex) {
+        } catch (HibernateException ex) {
             ex.printStackTrace();
         } finally {
             if (session != null && session.isOpen()) {
@@ -25,7 +28,7 @@ public class AtmDAOimpl implements AtmDAO {
     public void updateCardinfoEntity(String id, CardinfoEntity cardinfoEntity) {
         Session session = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = getSessionFactory().openSession();
             session.beginTransaction();
             session.update(cardinfoEntity);
             session.getTransaction().commit();
@@ -42,7 +45,7 @@ public class AtmDAOimpl implements AtmDAO {
         Session session = null;
         CardinfoEntity cardinfoEntity = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = getSessionFactory().openSession();
             cardinfoEntity = session.load(CardinfoEntity.class, id);
         } catch (Exception ex) {
             ex.printStackTrace();
